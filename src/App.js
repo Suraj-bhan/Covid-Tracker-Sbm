3b56bdcb35803e6894d3ce5679f39ac1c2fec2d0
Suraj-bhan <2018mmb1296@iitrpr.ac.in>
2021-04-29 23:55:23 +0530

    added countries

--- src/App.js
+++ src/App.js
@@ -1,10 +1,54 @@
 
+import { MenuItem } from '@material-ui/core';
+import { FormControl, Select } from '@material-ui/core';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+//https://disease.sh/v3/covid-19/countries
+
 function App() {
+  const [countries, setCountries] = useState([]);
+   
+  useEffect(() => {
+    const getCountriesData = async () => {
+     await fetch("https://disease.sh/v3/covid-19/countries")
+     .then((response) => response.json())
+     .then((data) =>{
+       const countries = data.map((country) => 
+       (
+         {
+           name:country.country,
+           valur: country.countryInfo.iso2
+         }
+       ));
+
+         setCountries(countries);
+     });
+    };
+         getCountriesData();
+
+  }, []);
+
   return (
     <div className="App">
-     <h1>let's build a covid 19 tracker</h1>
+      <div className="app_header">
+
+      
+     <h1>COVID-19 TRACKER</h1>
+     <FormControl className="app_dropdown">
+      <Select variant="outlined" value="abc">
+
+       {
+         countries.map((country) =>(
+           <MenuItem value={country.value}>{country.name}</MenuItem>
+         ))
+       }
+
+      </Select>
+
+     </FormControl>
+     </div>
+
     </div>
   );
 }
